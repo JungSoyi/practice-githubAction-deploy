47933b8feb7ae761d7e1953e49ea5d98f4bf7de4
JungSoyi <104415354+JungSoyi@users.noreply.github.com>
2022-10-07 11:43:57 +0900

    Add files via upload

---
+++ src/test/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,15 @@
+package com.codestates.practicegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+// 왜안돼
